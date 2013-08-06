b5eaf8067d8ffb5639e8bb38e22d76c6550dda84
Johannes <jwalter@student.neumont.edu>
2013-08-06 08:02:59 -0600

    Update SaveLayers.jsx

--- SaveLayers.jsx
+++ SaveLayers.jsx
@@ -1,30 +1,12 @@
 // NAME: 
-//  SaveLayers
+// 	SaveLayers
 
 // DESCRIPTION: 
 //	Saves each layer in the active document to a PNG or JPG file named after the layer. 
-//	These files will be created in the current document folder.
+//	These files will be created in the current document folder (same as working PSD).
 
 // REQUIRES: 
-//	Adobe Photoshop CS2 or higher
-
-// VERSIONS:
-// 	24 May 2013 by Johannes Walter  (graphicdesign.stackexchange/users/408/johannes)
-// 		Nesting properly handled
-// 		All layers save seperately again (no more stacking).
-
-//	27 March 2013 by Robin Parmar (robin@robinparmar.com)
-//		preferences stored in object
-//		auto-increment file names to prevent collisions
-//		properly handles layer groups
-//		header added
-//		code comments added
-//		main() now has error catcher
-//		counts number of layers
-//		many little code improvements
-
-//	26 Sept 2012 by Johannes Walter on stackexchange (graphicdesign.stackexchange/users/408/johannes)
-//		Original version
+// 	Adobe Photoshop CS2 or higher
 
 // enable double-clicking from Finder/Explorer (CS2 and higher)
 #target photoshop
