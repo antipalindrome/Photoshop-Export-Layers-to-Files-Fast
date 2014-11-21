96bc2e2c7635652e9de7367280bbdee6ff21bde0
Justin Godesky <justingodesky@gmail.com>
2014-11-21 03:36:21 -0500

    Ignore layers with bang at beginning of name.

--- Export Layers To Files (Fast).jsx
+++ Export Layers To Files (Fast).jsx
@@ -264,6 +264,9 @@ function exportLayers(visibleOnly, progressBarWindow)
 		for (var i = 0; i < count; ++i) {
 			var layer = layersToExport[i].layer;
 
+			// Ignore layers that have a bang in front, ie: "not".
+			if (layer.name.indexOf('!') === 0) continue;
+
 			var fileName;
 			switch (prefs.naming) {
 
