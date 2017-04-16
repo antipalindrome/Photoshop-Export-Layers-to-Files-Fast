aa311402d1ebae92da2cecfa27b94fcff6627718
Crt Gregoric <crt.gregoric@gmail.com>
2017-04-16 18:07:42 +0200

    Added file output suffix functionality

--- Export Layers To Files (Fast).jsx
+++ Export Layers To Files (Fast).jsx
@@ -138,6 +138,7 @@ var DEFAULT_SETTINGS = {
 	allowSpaces: app.stringIDToTypeID("allowSpaces"),
 	letterCase:	app.stringIDToTypeID("letterCase"),
 	outputPrefix: app.stringIDToTypeID("outputPrefix"),
+	outputSuffix: app.stringIDToTypeID("outputSuffix"),
 	trim: app.stringIDToTypeID("trim"),
 	exportBackground: app.stringIDToTypeID("exportBackground"),
 	fileType: app.stringIDToTypeID("fileType"),
@@ -188,6 +189,7 @@ function main()
 	prefs.formatArgs = null;
 	prefs.exportLayerTarget = ExportLayerTarget.ALL_LAYERS;
 	prefs.outputPrefix = "";
+	prefs.outputSuffix = "";
 	prefs.naming = FileNameType.AS_LAYERS_NO_EXT;
 	prefs.namingLetterCase = LetterCase.KEEP;
 	prefs.replaceSpaces = true;
@@ -621,11 +623,12 @@ function makeFileNameFromLayerName(layer, stripExt)
 function getUniqueFileName(fileName, layer)
 {
 	var ext = prefs.fileExtension;
-	// makeValidFileName() here basically just converts the space between the prefix and the core file name,
+	// makeValidFileName() here basically just converts the space between the prefix, the core file name and suffix,
 	// but it's a good idea to keep file naming conventions in one place, i.e. inside makeValidFileName(),
 	// and rely on them exclusively.
 	var outputPrefix = prefs.groupsAsFolders ? "" : prefs.outputPrefix;
-	fileName = makeValidFileName(outputPrefix + fileName, prefs.replaceSpaces);
+	var outputSuffix = prefs.groupsAsFolders ? "" : prefs.outputSuffix;
+	fileName = makeValidFileName(outputPrefix + fileName + outputSuffix, prefs.replaceSpaces);
 	if (prefs.namingLetterCase == LetterCase.LOWERCASE) {
 		fileName = fileName.toLowerCase();
 		ext = ext.toLowerCase();
@@ -906,9 +909,15 @@ function showDialog()
 		this.text = makeValidFileName(this.text, prefs.replaceSpaces);
 	};
 
+	// file name suffix
+	dlg.funcArea.content.grpSuffix.editSuffix.onChange = function() {
+		this.text = makeValidFileName(this.text, prefs.replaceSpaces);
+	};
+
 	dlg.funcArea.content.grpPrefix.cbFolderTree.onClick = function()
 	{
 		dlg.funcArea.content.grpPrefix.editPrefix.enabled = !this.value;
+		dlg.funcArea.content.grpSuffix.editSuffix.enabled = !this.value;
 	};
 
 	// file naming options
@@ -942,6 +951,11 @@ function showDialog()
 		prefs.outputPrefix = dlg.funcArea.content.grpPrefix.editPrefix.text;
 		if (prefs.outputPrefix.length > 0) {
 			prefs.outputPrefix += " ";
+		}
+
+		prefs.outputSuffix = dlg.funcArea.content.grpSuffix.editSuffix.text;
+		if (prefs.outputSuffix.length > 0) {
+			prefs.outputSuffix += " ";
 		}
 
 		prefs.groupsAsFolders = dlg.funcArea.content.grpPrefix.cbFolderTree.value;
@@ -1039,6 +1053,9 @@ function applySettings(dlg, formatOpts)
 			grpPrefix.cbFolderTree.notify();
 		}
 
+		grpSuffix.editSuffix.text = settings.outputSuffix;
+		grpSuffix.editSuffix.notify("onChange");
+
 		var drdTrimIdx = TrimPrefType.getIndex(settings.trim);
 		grpTrim.drdTrim.selection = (drdTrimIdx >= 0) ? drdTrimIdx : 0;
 		grpTrim.cbTrim.value = settings.forceTrimMethod;
@@ -1091,6 +1108,7 @@ function saveSettings(dlg, formatOpts)
 		desc.putInteger(DEFAULT_SETTINGS.letterCase, LetterCase.forIndex(grpLetterCase.drdLetterCase.selection.index));
 		desc.putString(DEFAULT_SETTINGS.outputPrefix, grpPrefix.editPrefix.text);
 		desc.putBoolean(DEFAULT_SETTINGS.groupsAsFolders, grpPrefix.cbFolderTree.value);
+		desc.putString(DEFAULT_SETTINGS.outputSuffix, grpSuffix.editSuffix.text);
 		desc.putInteger(DEFAULT_SETTINGS.trim, TrimPrefType.forIndex(grpTrim.drdTrim.selection.index));
 		desc.putBoolean(DEFAULT_SETTINGS.exportBackground, cbBgLayer.value);
 		desc.putString(DEFAULT_SETTINGS.fileType, formatOpts[grpFileType.drdFileType.selection.index].opt.type);
@@ -1132,6 +1150,7 @@ function getSettings(formatOpts)
 			letterCase: desc.getInteger(DEFAULT_SETTINGS.letterCase),
 			outputPrefix: desc.getString(DEFAULT_SETTINGS.outputPrefix),
 			groupsAsFolders: desc.getBoolean(DEFAULT_SETTINGS.groupsAsFolders),
+			outputSuffix: desc.getString(DEFAULT_SETTINGS.outputSuffix),
 			trim: desc.getInteger(DEFAULT_SETTINGS.trim),
 			exportBackground: desc.getBoolean(DEFAULT_SETTINGS.exportBackground),
 			fileType: desc.getString(DEFAULT_SETTINGS.fileType),
